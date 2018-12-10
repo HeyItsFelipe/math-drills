711164405a07b682dbe2010c5c0011790a73765a
Felipe Fernandez <heyitsfelipe@gmail.com>
2018-12-09 20:24:49 -0800

    Adds Results css.

--- client/src/Results.js
+++ client/src/Results.js
@@ -1,17 +1,23 @@
 import React from 'react';
+import './Results.css';
 
 const Results = ({ problems, answers, userAnswers }) => {
+    let totalCorrect = 0;
     let allResults = problems.map((element, index) => {
         if (answers[index] === userAnswers[index]['value']) {
-            return (<li key={userAnswers[index]['key']}>{element} {userAnswers[index]['value']}</li>);
+            totalCorrect++;
+            return (<li className="Results-correct" key={userAnswers[index]['key']}>{element} {userAnswers[index]['value']}</li>);
         } else {
-            return (<li key={userAnswers[index]['key']}>{element} {userAnswers[index]['value']}***</li>);
+            return (<li className="Results-incorrect" key={userAnswers[index]['key']}>{element} {userAnswers[index]['value']}</li>);
         }
     });
+    let score = (totalCorrect / problems.length).toFixed(4) * 100;
     return (
         <div className="Results">
-            <div>Results</div>
-            <ul>{allResults}</ul>
+            <div className="Results-title">Score {score}%</div>
+            <div>
+                <ul className="Results-list">{allResults}</ul>
+            </div>
         </div>
     );
 };
