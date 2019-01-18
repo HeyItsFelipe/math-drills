702e1f2d2601f1f81d61d5a74957e45776e6ef6a
Felipe Fernandez <heyitsfelipe@gmail.com>
2019-01-18 15:05:44 -0800

    Updates css for results component

--- client/src/Results.js
+++ client/src/Results.js
@@ -4,19 +4,39 @@ import './Results.css';
 const Results = ({ problems, answers, userAnswers }) => {
     let totalCorrect = 0;
     let allResults = problems.map((element, index) => {
+        console.log(element.split(' '));
+        let splittedProblem = element.split(' ');
         if (answers[index] === userAnswers[index]['value']) {
             totalCorrect++;
-            return (<li className="Results-correct" key={userAnswers[index]['key']}>{element} {userAnswers[index]['value']}</li>);
+            return (
+                <li className="Results-correct" key={userAnswers[index]['key']}>
+                    <div className="Results-number">{index + 1}.</div>
+                    <div className="Results-problem-element">{splittedProblem[0]}</div>
+                    <div className="Results-problem-element">{splittedProblem[1]}</div>
+                    <div className="Results-problem-element">{splittedProblem[2]}</div>
+                    <div className="Results-problem-element">{splittedProblem[3]}</div>
+                    <div className="Results-problem-element">{userAnswers[index]['value']}</div>
+                </li>);
         } else {
-            return (<li className="Results-incorrect" key={userAnswers[index]['key']}>{element} {userAnswers[index]['value']}</li>);
+            return (
+                <li className="Results-incorrect" key={userAnswers[index]['key']}>
+                    <div className="Results-number">{index + 1}.</div>
+                    <div className="Results-problem-element">{splittedProblem[0]}</div>
+                    <div className="Results-problem-element">{splittedProblem[1]}</div>
+                    <div className="Results-problem-element">{splittedProblem[2]}</div>
+                    <div className="Results-problem-element">{splittedProblem[3]}</div>
+                    <div className="Results-problem-element">{userAnswers[index]['value']}</div>
+                </li>);
         }
     });
     let score = (totalCorrect / problems.length).toFixed(4) * 100;
     return (
         <div className="Results">
-            <div className="Results-title">Score {score}%</div>
-            <div>
-                <ul className="Results-list">{allResults}</ul>
+            <div className="Results-inner">
+                <div className="Results-title">Score: {score}%</div>
+                <div>
+                    <ul className="Results-list">{allResults}</ul>
+                </div>
             </div>
         </div>
     );
