36455426eb933beaf20bc3159d49b43b7640cc21
Felipe Fernandez <heyitsfelipe@gmail.com>
2019-01-22 10:40:35 -0800

    Cleans code and styling, especially App component

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 import MultiplicationDrill from './MultiplicationDrill';
 import Menu from './Menu';
@@ -8,12 +7,14 @@ import { BrowserRouter as Router, Route } from 'react-router-dom';
 class App extends Component {
   render() {
     return (
-      <Router>
-        <div>
-          <Route exact path="/" component={Menu} />
-          <Route exact path="/multiplication-drill" component={MultiplicationDrill} />
-        </div>
-      </Router>
+      <div className="App">
+        <Router>
+          <div>
+            <Route exact path="/" component={Menu} />
+            <Route exact path="/multiplication-drill" component={MultiplicationDrill} />
+          </div>
+        </Router>
+      </div>
     );
   }
 }
